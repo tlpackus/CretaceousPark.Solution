00a9262c9a4a053620e4633528fdbbc1a88d9212
tlpackus <tlpackus@gmail.com>
2021-06-20 12:18:25 -0700

    add get route

---
+++ CretaceousPark/Migrations/20210620184611_UpdateAppSettingsjson.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CretaceousPark.Migrations
+{
+    public partial class UpdateAppSettingsjson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
